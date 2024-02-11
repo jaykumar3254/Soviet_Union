4366b62460cbb0587dbcb93bf28ef8edc7a05354
jaykumar3254 <137028327+jaykumar3254@users.noreply.github.com>
2024-02-12 00:58:52 +0530

    12:58
    for arrays

--- third.cpp
+++ third.cpp
@@ -1,23 +1,42 @@
-/*//arrays and string functions 
+//arrays and string functions 
 #include<iostream>
 
 using namespace std ;
 
 int main()
 {
-    int i, arr[5]={10,20,12,13,40};
-    for(i=0;i<5;i++)
+    int i, arr[]={10,20,1,5,2,12,13,40,8,9,7,5,4,4,3};
+    for(i=0;i<sizeof(arr)/sizeof(arr[0]);i++)
     {
         cout<<arr[i]<<endl;
     }
  return 0;
 }
-*/
+
+/*
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-    
-}
+    int arr[5];
+    arr [0]= 23; //dynamically adding element in array
+    cout<<arr[0];
+    return 0;
+}*//*
+//functions in array 
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    int arr[5];
+
+    cout<<" size fo array : "<<sizeof(arr)<<endl;
+    int l =sizeof(arr)/ sizeof(arr[0]);
+    cout<<l;
+
+    return 0;
+}*/
