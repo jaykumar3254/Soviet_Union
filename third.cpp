b04c4f15fb7eb99bf9d6ca19b53c42bbdfbbcde3
jaykumar3254 <137028327+jaykumar3254@users.noreply.github.com>
2024-11-23 00:01:51 +0530

    22
    nothing

--- third.cpp
+++ third.cpp
@@ -8,7 +8,7 @@ int main()
     int i, arr[]={10,20,1,5,2,12,13,40,8,9,7,5,4,4,3};
     for(i=0;i<sizeof(arr)/sizeof(arr[0]);i++)
     {
-        cout<<arr[i]<<endl;
+        cout<<arr[2]<<endl;
     }
  return 0;
 }
